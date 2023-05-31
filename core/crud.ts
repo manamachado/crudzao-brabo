19f6ed620b72f8a6d37679d38c38e4c70e4e7142
Maná Machado <manamachado8@gmail.com>
2023-05-31 00:59:15 -0300

    aula 2.1 - create - 2

---
+++ core/crud.ts
@@ -0,0 +1,12 @@
+import fs from 'fs';
+const DB_FILE_PATH = './core/db';
+
+console.log("[crud]");
+
+function create(content: string) {
+  fs.writeFileSync(DB_FILE_PATH, content);
+  return content;
+}
+
+// SIMULATION
+console.log(create('created!'));
