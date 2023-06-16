e1feb60510c6bc7b8390d0195c9f9ca073089675
Maná Machado <manamachado8@gmail.com>
2023-06-16 17:00:34 -0300

    aula 2.2 - read

--- core/crud.ts
+++ core/crud.ts
@@ -3,10 +3,49 @@ const DB_FILE_PATH = './core/db';
 
 console.log('[crud]');
 
+interface Todo {
+  date: string;
+  content: string;
+  done: boolean;
+}
+
 function create(content: string) {
-  fs.writeFileSync(DB_FILE_PATH, content);
-  return content;
+  const todo: Todo = {
+    date: new Date().toISOString(),
+    content: content,
+    done: false
+  }
+
+  const todos: Array<Todo> = [
+    ...read(),
+    todo
+  ]
+
+  fs.writeFileSync(DB_FILE_PATH, JSON.stringify({
+    todos,
+    dogs: []
+  }, null, 2));
+
+  return todos;
+}
+
+function read(): Array<Todo> {
+  const dbString = fs.readFileSync(DB_FILE_PATH, 'utf-8');
+  const db = JSON.parse(dbString || '{}');
+
+  if (!db.todos) {
+    return [];
+  }
+  
+  return db.todos;
+}
+
+function CLEAR_DB() {
+  fs.writeFileSync(DB_FILE_PATH, '');
 }
 
 // SIMULATION
-console.log(create('created!'));
+CLEAR_DB();
+console.log(create('Primeira TODO'));
+console.log(create('Segunda TODO'));
+console.log(read());
